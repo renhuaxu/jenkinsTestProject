8d836df0c4f3db51185cd264c34e283e93a5916e
elwin <renhuaxu@163.com>
2018-08-31 10:09:31 +0800

    test
    项目初始化

---
+++ AudioRecordAndCrop/AudioRecordAndCrop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioRecordAndCrop
+//
+//  Created by renhua xu on 2018/8/2.
+//  Copyright © 2018年 scrcu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioRecordAndCrop/AudioRecordAndCrop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioRecordAndCrop
+//
+//  Created by renhua xu on 2018/8/2.
+//  Copyright © 2018年 scrcu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
